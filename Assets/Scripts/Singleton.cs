6a259f9b33df64bca70e06006f31fb6397d0ee09
hugo <hugohuang1111@gmail.com>
2022-06-13 00:45:43 +0800

    add sington

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour
+    where T : MonoBehaviour
+{
+    private static T gInstance = null;
+
+    public static T Instance
+    {
+        get { return gInstance; }
+    }
+
+    protected virtual void Awake()
+    {
+        gInstance = this as T;
+    }
+}
